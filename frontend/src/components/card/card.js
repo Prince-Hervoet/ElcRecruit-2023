c0cf25e4e165aec0b72986beca95b14e0ebb3f01
Prince-Hervoet <2064128441@qq.com>
2023-06-25 04:00:26 +0800

    fix: #add files

--- frontend/src/components/card/card.js
+++ frontend/src/components/card/card.js
@@ -7,21 +7,11 @@ export default function Card({ colorFlag, title, content }) {
   return (
     <div className="card-body" style={{ borderLeft: "5px solid red" }}>
       <div className="card-title">
-        <div className="card-title-name">
-          {/* <div
-            style={{
-              width: 18,
-              height: 18,
-              backgroundColor: "red",
-              marginRight: 5,
-              borderRadius: 6,
-              border: "3px solid black",
-            }}
-          ></div> */}
-          <span>振炫哥哥</span>
-        </div>
         <div className="card-title-dep">
           <span>维修部</span>
+        </div>
+        <div className="card-title-name">
+          <span>振炫哥哥</span>
         </div>
       </div>
       <div className="card-content"></div>
