9e30296f4f61de75cdcbce7da203f7fe78543e7f
振炫 邱 <2296282722@qq.com>
2023-08-25 22:51:21 +0800

    新建身份验证项目

--- backend/interviewer/Controllers/InterviewController.cs
+++ backend/interviewer/Controllers/InterviewController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +12,11 @@ namespace interviewer.Controllers
     [ApiController]
     public class InterviewController : ControllerBase
     {
+        [HttpGet]
+        [Authorize]
+        public IActionResult Hello()
+        {
+            return Ok("Hello IdentityServer4!");
+        }
     }
 }
