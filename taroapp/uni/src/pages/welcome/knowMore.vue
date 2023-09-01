0061b1cee7e07444e22425d5739822e8e7dd9c86
Prince-Hervoet <2064128441@qq.com>
2023-09-01 17:28:29 +0800

    fix: #add codes

--- taroapp/uni/src/pages/welcome/knowMore.vue
+++ taroapp/uni/src/pages/welcome/knowMore.vue
@@ -1,51 +1,59 @@
 <template>
-  <view class="IntroPic"
-    ><img class="elcphoto" src="C:\Users\86139\Desktop\elcphoto.jpg" alt=""
-  /></view>
-  <br /><br /><br />
-  <view class="KnowDep">
-    <view class="DepBigBox">
-      <img src="C:\Users\86139\Downloads\pcb.png" alt="" />
-      电协介绍</view
-    >
-    <view class="ali" id="First">
-      <view class="DepBox" @click="gotoCoding(5)">
-        <img src="C:\Users\86139\Downloads\contact.png" alt="" />
-        外联部</view
-      >
+  <view class="knowMore-container">
+    <view class="IntroPic">
+      <img class="elcphoto" src="/static/ya.png" alt="" />
+    </view>
+    <br /><br /><br />
+    <view class="KnowDep">
+      <view class="DepBigBox">
+        <img src="/static/pcb.png" alt="" />
+        电协介绍
+      </view>
+      <view class="ali" id="First">
+        <view class="DepBox" @click="gotoCoding(5)">
+          <img src="/static/contact.png" alt="" />
+          外联部
+        </view>
+        <br />
+        <view class="DepBox" @click="gotoCoding(1)">
+          <img src="/static/repair.png" alt="" />
+          维修部
+        </view>
+        <br />
+      </view>
       <br />
-      <view class="DepBox" @click="gotoCoding(1)">
-        <img src="C:\Users\86139\Downloads\repair.png" alt="" />
-        维修部</view
-      >
-      <br />
-    </view>
-    <br />
-    <view class="ali" id="Second">
-      <view class="DepBox" @click="gotoCoding(4)">
-        <img src="C:\Users\86139\Downloads\publicize.png" alt="" />
-        网宣部</view
-      >
-      <view class="DepBox" @click="gotoCoding(3)">
-        <img src="C:\Users\86139\Downloads\project.png" alt="" />
-        项目部</view
-      >
-    </view>
-    <view class="ali" id="Third">
-      <view class="DepBox" @click="gotoCoding(6)">
-        <img src="C:\Users\86139\Downloads\practice.png" alt="" />
-        实践部</view
-      >
-      <view class="DepBox" @click="gotoCoding(2)">
-        <img src="C:\Users\86139\Downloads\secretary.png" alt="" />
-        秘书部</view
-      >
-    </view>
+      <view class="ali" id="Second">
+        <view class="DepBox" @click="gotoCoding(4)">
+          <img src="/static/publicize.png" alt="" />
+          网宣部</view
+        >
+        <view class="DepBox" @click="gotoCoding(3)">
+          <img src="/static/project.png" alt="" />
+          项目部
+        </view>
+      </view>
+      <view class="ali" id="Third">
+        <view class="DepBox" @click="gotoCoding(6)">
+          <img src="/static/practice.png" alt="" />
+          实践部
+        </view>
+        <view class="DepBox" @click="gotoCoding(2)">
+          <img src="/static/secretary.png" alt="" />
+          秘书部
+        </view>
+      </view>
 
-    <button class="DepZuBox" @click="gotoCoding(7)">
-      <img src="C:\Users\86139\Downloads\coding.png" alt="" />
-      软件组
-    </button>
+      <view class="ali" id="Third">
+        <view class="DepBox" @click="gotoCoding(6)">
+          <img src="/static/coding.png" alt="" />
+          软件组
+        </view>
+        <view class="DepBox" @click="gotoCoding(2)" style="visibility: hidden">
+          <img src="" alt="" />
+          占位组
+        </view>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -56,11 +64,16 @@ const gotoCoding = (depId) => {
   });
 };
 </script>
-<style>
+<style scoped>
+.knowMore-container {
+  padding-bottom: 20px;
+}
+
 .elcphoto {
   width: 100%;
   height: 350px;
 }
+
 image {
   width: 20px;
   height: 20px;
@@ -76,7 +89,7 @@ image {
   margin: auto;
   background-color: rgb(255, 255, 255);
   border-radius: 1em;
-  box-shadow: 1px 1px 13px 3px rgba(176, 179, 179, 0.329);
+  box-shadow: 1px 1px 13px 2px rgba(221, 221, 221, 0.329);
   display: flex;
   align-items: center;
   justify-content: center;
@@ -93,7 +106,7 @@ image {
   margin: auto;
   background-color: rgb(255, 255, 255);
   border-radius: 1em;
-  box-shadow: 1px 1px 13px 3px rgba(176, 179, 179, 0.329);
+  box-shadow: 1px 1px 13px 2px rgba(221, 221, 221, 0.329);
   display: flex;
   align-items: center;
   justify-content: center;
@@ -108,15 +121,13 @@ image {
 .DepZuBox {
   width: 45%;
   height: 70px;
-  text-align: center;
   margin-left: 2%;
   background-color: rgb(255, 255, 255);
   border-radius: 1em;
-  box-shadow: 1px 1px 13px 3px rgba(176, 179, 179, 0.329);
+  box-shadow: 1px 1px 13px 2px rgba(221, 221, 221, 0.329);
   display: flex;
   align-items: center;
   justify-content: center;
-  transition: font-size 0.5s;
   font-size: 25px;
   font-family: 楷体;
   font-weight: 700;
@@ -124,7 +135,6 @@ image {
   margin-bottom: 50px;
 }
 .ali {
-  /* display: grid; */
   display: flex;
   justify-content: center;
   align-items: center;
