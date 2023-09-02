17c888bb7c4c93d3705d6ddecf85add978acfdf7
Prince-Hervoet <2064128441@qq.com>
2023-09-02 21:54:38 +0800

    fix: #add codes

--- taroapp/uni/src/pages/welcome/welcome.vue
+++ taroapp/uni/src/pages/welcome/welcome.vue
@@ -1,14 +1,18 @@
 <template>
   <view class="welcome-container">
-    <view class="welcome-recruit-button-container">
-      <button class="welcome-button-recruit" @click="clickGotoRecruit">
-        <span style="font-size: medium; font-weight: 700">报名入口</span>
-      </button>
-    </view>
-    <view class="welcome-introwe-button-container">
-      <button class="welcome-button-knowus" @click="clickGotoknow">
-        <span style="font-size: medium; font-weight: 700">了解我们</span>
-      </button>
+    <view class="welcome-button-container">
+      <view style="margin-right: 20px;">
+        <button class="welcome-button-knowus" @click="clickGotoknow">
+          <img src="/static/knowUs.png" style="width: 20px;height:20px;margin-right:5px">
+          <span style="font-size: medium; font-weight: 700">了解我们</span>
+        </button>
+      </view>
+      <view>
+        <button class="welcome-button-recruit" @click="clickGotoRecruit">
+          <img src="/static/recruit.png" style="width: 22px;height:22px;margin-right:5px">
+          <span style="font-size: medium; font-weight: 700">报名入口</span>
+        </button>
+      </view>
     </view>
   </view>
 </template>
@@ -28,39 +32,47 @@ const clickGotoknow = () => {
 
 <style scoped>
 .welcome-button-recruit {
-  width: 240px;
-  height: 45px;
+  width: 120px;
+  height: 100px;
   background-color: rgba(209 54 57);
   display: flex;
   align-items: center;
   justify-content: center;
-  border-radius: 6px;
+  border-radius: 12px;
   color: #ffffff;
+  transition: 0.1s all;
+  box-shadow: 1px 2px 10px rgba(209, 54, 57, 0.574);
 }
+
+.welcome-button-recruit:active {
+  transform: scale(1.2, 1.2);
+}
+
 .welcome-button-knowus {
-  width: 240px;
-  height: 45px;
+  width: 120px;
+  height: 100px;
   background-color: rgba(221, 220, 220, 0.87);
   display: flex;
   align-items: center;
   justify-content: center;
-  border-radius: 6px;
+  border-radius: 12px;
   color: #000000;
+  box-shadow: 1px 2px 10px rgba(221, 220, 220, 0.586);
 }
 
-.welcome-recruit-button-container {
-  position: fixed;
-  bottom: 100px;
-  left: 50%;
-  transform: translate(-50%, -50%);
+.welcome-button-knowus:active {
+  transform: scale(1.2, 1.2);
 }
 
-.welcome-introwe-button-container {
+.welcome-button-container {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   position: fixed;
-  bottom: 160px;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  bottom: 150px;
 }
+
 
 .welcome-container {
   height: 100vh;
