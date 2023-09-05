2596a7a84073f1673d67a5543a7b18431692c9ea
Prince-Hervoet <2064128441@qq.com>
2023-09-05 17:55:35 +0800

    fix: #add codes

---
+++ frontend/src/requests/resumeRequest.js
@@ -0,0 +1,34 @@
+import { ServiceUrls } from "./util";
+
+/**
+ * 获取学生个人信息
+ * @param {*} userId
+ */
+async function sendGetStudentInfo(userId) {}
+
+/**
+ * 获取所有评论
+ * @param  userId
+ */
+async function sendGetComments(userId) {}
+
+/**
+ * 更新学生状态
+ * @param {*} nStatus
+ */
+async function sendUpdateStudentStatus(nStatus) {}
+
+/**
+ * 提交评论和评分
+ * @param {*} comment
+ * @param {*} score
+ * @param {*} userName
+ */
+async function sendCommentAndScore(comment, score, userName) {}
+
+export const ResumeRequest = {
+  sendCommentAndScore,
+  sendStartInterview,
+  sendGetComments,
+  sendUpdateStudentStatus,
+};
