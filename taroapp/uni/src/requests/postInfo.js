257cf6b7814e118ed936de67ac50bc19e09763b2
yinyan1227@qq.com <yinyan1227@qq.com>
2023-09-18 23:33:55 +0800

    add code

--- taroapp/uni/src/requests/postInfo.js
+++ taroapp/uni/src/requests/postInfo.js
@@ -9,7 +9,7 @@ export async function sendSubmitForm(sendObj) {
 //提交表单的请求
 // clickSubmitForm.onclick = function () {
 //   axios
-//     .post("/user")
+//     .post("elc_recruit/student/commit")
 //     .then(function (response) {
 //       console.log(response);
 //     })
@@ -17,6 +17,22 @@ export async function sendSubmitForm(sendObj) {
 //       console.log(error);
 //     });
 // };
+
+wx.login({
+  success(res) {
+    if (res.code) {
+      //发起网络请求
+      wx.request({
+        url: "http://192.168.123.86:8081/elc_recruit/interviewer/WeChatLogin",
+        data: {
+          code: res.code,
+        },
+      });
+    } else {
+      console.log("登录失败！" + res.errMsg);
+    }
+  },
+});
 
 export async function sendGetInfo(openId) {
   const url = ServiceUrls.getInfo + `?openId=${openId}`;
