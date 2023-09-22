d467a702adba41e52659e08213b2c25c9677cb0b
振炫 邱 <2296282722@qq.com>
2023-09-22 21:46:16 +0800

    feat: add uview-plus lib

--- taroapp/uni/src/main.js
+++ taroapp/uni/src/main.js
@@ -1,7 +1,8 @@
+import uviewPlus from 'uview-plus'
 import { createSSRApp } from "vue";
 import App from "./App.vue";
 export function createApp() {
-  const app = createSSRApp(App);
+  const app = createSSRApp(App).use(uviewPlus);
   return {
     app,
   };
