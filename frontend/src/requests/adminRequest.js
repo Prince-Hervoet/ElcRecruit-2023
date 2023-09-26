aa2952a60ffbc9afb8d98c4adb10fc2ee8430a43
Prince-Hervoet <2064128441@qq.com>
2023-09-26 22:35:45 +0800

    feat: #add codes

--- frontend/src/requests/adminRequest.js
+++ frontend/src/requests/adminRequest.js
@@ -24,9 +24,49 @@ async function getCurrentProcess() {
 /**
  * 推进进度
  */
-async function pushProcess() {}
+async function pushProcess(isForce) {
+  const url = ServiceUrls.pushCurrentProcess + `?force=${isForce}`;
+  let ans = {};
+  try {
+    const res = await axios({
+      method: "POST",
+      url,
+    });
+    ans.success = true;
+    ans.data = res.data;
+  } catch (e) {
+    ans.success = false;
+    ans.data = e;
+  }
+  return ans;
+}
+
+async function exportMenu(status, depId) {
+  let url = ServiceUrls.exportMenu;
+  if (status && depId) {
+    url += `?studentState=${status}&department=${depId}`;
+  }
+  let ans = {};
+  try {
+    const res = await axios({
+      method: "GET",
+      url,
+    });
+    const blob = new Blob([res.data], { type: "application/vnd.ms-excel" });
+    const objectUrl = URL.createObjectURL(blob);
+    window.location.href = objectUrl;
+    URL.revokeObjectURL(objectUrl);
+    ans.success = true;
+    ans.data = res.data;
+  } catch (e) {
+    ans.success = false;
+    ans.data = e;
+  }
+  return ans;
+}
 
 export const AdminRequest = {
   getCurrentProcess,
   pushProcess,
+  exportMenu,
 };
