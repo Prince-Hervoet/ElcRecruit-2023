41894941e53d50413784723432684f0910d5b67f
Prince-Hervoet <2064128441@qq.com>
2023-10-08 23:24:47 +0800

    merge

--- newElcZx/newElcRecruit/src/router.js
+++ newElcZx/newElcRecruit/src/router.js
@@ -6,12 +6,9 @@ import UserInfoForm from "./views/UserInfoForm.vue";
 import DepIntros from "./views/DepIntros.vue";
 import KnowElc from "./views/KnowElc.vue";
 import NewWelcome from "./views/NewWelcome.vue";
-<<<<<<< HEAD
 import ForgetPassword from "./views/ForgetPassword.vue";
-=======
 import Welcome from "./views/Welcome.vue";
 
->>>>>>> 84fe03d11ea6bfa86a8f636a851719f8b2772da2
 const routes = [
   {
     path: "/",
