2a4c1de1f07427be570f8bfb58e2db791f23932f
HeChuQIU <2296282722@qq.com>
2023-10-11 18:33:38 +0800

    111

--- newElcZx/newElcRecruit/src/views/DepIntros.vue
+++ newElcZx/newElcRecruit/src/views/DepIntros.vue
@@ -2,7 +2,7 @@
     <div class="gradient">
         <div class="gradient-content-body">
             <div class="depName">{{ depName }}</div>
-            <div class="smallIntro" @click="moreIntro">{{ smallIntro }} </div>
+            <div class="smallIntro" @click="toDepUrl">{{ smallIntro }} </div>
             <div class="depText">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
                 {{ depText }}
             </div>
@@ -18,7 +18,7 @@ const route = useRoute()
 const depName = ref("");
 const smallIntro = ref("");
 const depText = ref("");
-const depUrl = reactive({
+const depUrls = reactive({
     elc: "https://mp.weixin.qq.com/s/0GDsNOfe9BNjMMGUoMiOgA",
     repair: "https://mp.weixin.qq.com/s/n6Ibc3jWk7CSMsfQYTNXdQ",
     secretary: "https://mp.weixin.qq.com/s/j5JnYg5dRG9ZNWrYypKTcA",
@@ -28,11 +28,15 @@ const depUrl = reactive({
     practice: "https://mp.weixin.qq.com/s/VTp3I7Jr3o2OsNf_ZnuriA",
     coding: "https://mp.weixin.qq.com/s/9VVLQ2zdbtIY2bo_mI1Rvg",
 })
-function moreIntro(data) {
-    window.location.href = data;
+const depUrl=ref(""); 
+
+function setDepUrl(data) {
+    depUrl.value = data;
 }
 
-
+function toDepUrl(){
+    window.location.href = depUrl.value;
+}
 
 function showText(depId = "1") {
     switch (depId) {
@@ -50,7 +54,7 @@ function showText(depId = "1") {
                 "早晨的图书馆,凌晨的实验室",
                 "广东工业大学电子科技协会（简称广工电协）是由在校电子技术爱好者于2002年11月组织成立的，是广东工业大学最大型的学术性社团。在近20年中的发展中，协会不断突破自我，技术水平不断沉淀，自主研发室内机器人定位导航系统、基于PID算法的高效开关电源变换器、基于卷积神经网络的自动垃圾分类系统、实时数据流人脸识别的门禁系统，自主开发STM8和STM32开发板。同时电协开设贝尔组：分别在嵌入式领域、视觉领域、硬件领域、网络领域进行深入研究与开发。电协的理念是让平凡的人成为优秀的人，让优秀的人更加优秀。爱电子，爱生活。电协欢迎广大电子爱好者的加入，我们与你们共成长！"
             );
-            moreIntro(depUrl.elc)
+            setDepUrl(depUrls.elc)
             break;
         case "1":
             setFields(
@@ -58,7 +62,7 @@ function showText(depId = "1") {
                 "维物之态，修身之技",
                 "作为协会中最热闹也是最“卷”的部门之一，维修部当仁不让，在协会需要我们时挺身而出。我们维修部的工作日常是组织义修活动，维护协会元器件库以及处理学校内驿站的故障家电。义修活动的范围是在整个大学城校区，包括其他学校。"
             );
-            // moreIntro(depUrl.repair)
+            setDepUrl(depUrl.repair)
             break;
         case "2":
             setFields(
@@ -66,7 +70,7 @@ function showText(depId = "1") {
                 "秘才八斗，书气翩翩",
                 "秘书部主要负责文案撰写，活动申请以及协助准备各项比赛物资等工作；我们注重干事的技术提升，积极给予干事学习上的指导帮助；同时也经常开展团建活动，部门氛围非常和谐欢乐。"
             );
-            moreIntro(depUrl.secretary)
+            setDepUrl(depUrls.secretary)
             break;
         case "3":
             setFields(
@@ -74,7 +78,7 @@ function showText(depId = "1") {
                 "不远千里，项目等你",
                 "项目部是要对协会申请的项目进行审核以及相关资料的整理，同时收集整理协会成员比赛的相关资料以及获奖情况，并将其进行归档上传云盘，定期上传历年来的学习资料、试卷等。欢迎各位来到快乐的小卖部！！！"
             );
-            moreIntro(depUrl.project)
+            setDepUrl(depUrls.project)
             break;
         case "4":
             setFields(
@@ -82,7 +86,7 @@ function showText(depId = "1") {
                 "网宣之约，相见不散",
                 "作为电协对外宣传的窗口，在这里你不仅能学习如何运营公众号，制作推文，还能充分发挥你的想象力设计海报传单，剪辑视频！除了有各路硬件、嵌入式大佬带飞，让你快速脱离萌新期，成长为电协软硬件“大佬”。还有男女生节，生日庆祝等众多有趣好玩的团建活动等你来参加！在网宣，我们是队友，更是亲密无间的朋友，一网情深，宣之于口，网宣大家庭期待你的加入！"
             );
-            moreIntro(depUrl.pubilize)
+            setDepUrl(depUrls.pubilize)
             break;
         case "5":
             setFields(
@@ -90,7 +94,7 @@ function showText(depId = "1") {
                 "相约外联，逐梦电协",
                 "咱作为电协的脸面，必须得又好学又好玩！！！作为外联人，你可以：负责协会各个活动与会议的主持工作！策划举办社团活动，如部门破冰、科技比赛、男女生节、电协团建等！与校内外其他社团及社会资源建立联系，寻找交流机会和资源支持！负责电协课的物资、团建娱乐物资、比赛奖品等协会部分物质的购买与发放，负责社团招新和活动的宣传推广工作，利用多种渠道向校内外传递信息，提高社团的知名度和影响力！！！And...你还可以：跟着师兄师姐学习技术内容和相关经验！！！和协会的小伙伴们一起学习、一起参加比赛，提升自己！！！认识不同专业的永远最可爱的外联小伙伴！！！！！"
             );
-            moreIntro(depUrl.outreach)
+            setDepUrl(depUrls.outreach)
             break;
         case "6":
 
@@ -99,7 +103,7 @@ function showText(depId = "1") {
                 "躬身实践，力学笃行",
                 "实践部在日常工作中主要负责协会相关项目的整理收集，构建和完善协会的资料库，以及协助电协各类活动的摆摊工作等等，同时也参与开展相关的活动和比赛，并配合相关的宣传工作"
             );
-            moreIntro(depUrl.practice)
+            setDepUrl(depUrls.practice)
             break;
         case "7":
             setFields(
@@ -107,7 +111,7 @@ function showText(depId = "1") {
                 "代码，代码，还是代码",
                 "软件组衍生于贝尔组，致力于开发面向全校的项目和系统。软件组目前的方向有前端、后端、网络运维、小游戏开发，主要运用的语言有C/C++、javascript、java、go、C#、python等。已经开发的项目有gdutday、GDUTDays、电子科技协会论坛等。软件组始终坚持技术深耕，项目挖掘，致力于写出更优秀的代码以及实现更完美的业务。欢迎热爱coding的你加入我们！ (此报名网站便是由 @软件组成员yinyan && Hechu 呈献)"
             );
-            moreIntro(depUrl.coding)
+            setDepUrl(depUrls.coding)
             break;
     }
 };
@@ -119,8 +123,10 @@ function setFields(_depName, _smallIntro, _depText,) {
 };
 
 onMounted(() => {
-    console.log(route.params.depId);
-    showText(route.params.depId);
+    let depId = route.params.depId;
+    if(depId == "")
+        depId = "100";
+    showText(depId);
 })
 </script>
 
